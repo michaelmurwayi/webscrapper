3a50689b13e45e5e678902605a967633fb5dc9ff
michaelmurwayi <mikemurwayi7@gmail.com>
2021-11-04 09:46:49 +0300

    adding job in kenya scrapper and updating main and db files

---
+++ jobInKenya.py
@@ -0,0 +1,146 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import urllib
+import re
+import db
+
+# function to build URI
+def get_job_url(base_url):
+    # scrap hospitality link from home page job category
+    page = requests.get(base_url)
+    if page.status_code == 200:
+        soup = BeautifulSoup(page.content, "html.parser")
+        div = soup.find("ul", {"class": "dcw"}).find("li", {"class": "cat-item-27"})
+        url = (
+            str(div)
+            .replace('<li class="cat-item cat-item-27"><a href="', "")
+            .split('">')[0]
+        )
+
+    return url
+
+
+def web_scrap_job_links(pagination_list):
+    # get list of all jobs on the page
+    job_links = []
+
+    if isinstance(pagination_list, str):
+        page = requests.get(pagination_list)
+        if page.status_code == 200:
+            soup = BeautifulSoup(page.content, "html.parser")
+            div = soup.find("article", attrs={"class": "blog-view"}).find_all("a")
+            div.pop(0)
+
+            for data in div:
+
+                try:
+                    link = (
+                        re.search("%s(.*)%s" % ('"', '"'), str(data))
+                        .group(1)
+                        .split('href="')[1]
+                    )
+                    job_links.append(link)
+                except IndexError:
+                    return job_links
+            print(job_links)
+    else:
+        for URL in pagination_list:
+            page = requests.get(URL)
+            if page.status_code == 200:
+                soup = BeautifulSoup(page.content, "html.parser")
+                div = soup.find("div", attrs={"class": "fusion-posts-container"})
+            for data in div:
+                link = (
+                    re.search("%s(.*)%s" % ('"', '"'), str(data))
+                    .group(1)
+                    .split('href="')[1]
+                )
+                job_links.append(link)
+    return job_links
+
+
+def get_page_pagination(pagination_list):
+    # get all paginated pages in hospitatlity link
+    new = get_next_page(pagination_list[-1], pagination_list)
+    return "done"
+
+
+def get_next_page(URL, pagination_list):
+    # get next page in pagination list
+    page = requests.get(URL)
+    if page.status_code == 200:
+        soup = BeautifulSoup(page.content, "html.parser")
+        try:
+            next_page_url = soup.find("a", attrs={"class": "pagination-next"}).get(
+                "href"
+            )
+            pagination_list.append(next_page_url)
+            get_page_pagination(pagination_list)
+        except Exception:
+            # print(pagination_list)
+            # web_scrap_job_links(pagination_list)
+            return URL
+
+    return pagination_list
+
+
+def check_if_link_already_scrapped(url, table):
+    # check if link about to scan is already in db
+    # ignore link if in database
+    # scrapp link if not in database
+    stored_links = db.get_records(table)
+    if stored_links:
+        for items in stored_links:
+            if url in items:
+                return True
+            else:
+                return False
+    else:
+        return False
+
+
+def qualifications_scrapping(base_url):
+    # table for storage of scrapped links
+    table = base_url.split(".")[1] + "_url"
+
+    URL = get_job_url(base_url)
+
+    pagination_links = []
+    pagination_list = get_next_page(URL, pagination_links)
+    # get lists of all hospitality jobs available
+
+    job_links = web_scrap_job_links(pagination_list)
+
+    # Get all individual links from link
+    # make a request to get page data
+    data = []
+    for link in job_links:
+        if check_if_link_already_scrapped(link, table):
+            print("qualifications already scrapped from link")
+        else:
+            print(f"scrapping data from {link}")
+            page = requests.get(link)
+
+            # check url status code and proceed if code is 200 else terminate program
+            if page.status_code == 200:
+                soup = BeautifulSoup(page.content, "html.parser")
+                try:
+
+                    qualification_list = (
+                        soup.find("div", {"class": "entry-content"})
+                        .find_all("ul")[1]
+                        .text.split("\n")[1:-1]
+                    )
+                    results = {
+                        "url": link,
+                        "Qualification": qualification_list,
+                    }
+                    data.append(results)
+                except IndexError:
+                    results = {"url": link, "Qualification": "Problem with this link"}
+            else:
+                print("something went wrong, please check provided URL")
+                data = {}
+
+    return data
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
 import requests
+import schedule
+import time
 import db
 import brighterMondayScrapper
 import careersPointScrapper
 import jobsMagScrapper
+import jobInKenya
+import jobWebScrapper
 
 
 def get_input():
@@ -32,21 +36,62 @@ def scrap_data(url):
     if host_name == "brightermonday":
         # brighter monday scrapper module
         data = brighterMondayScrapper.qualifications_scrapping(url)
-        save_to_db(url, data)
+
+        if len(data) == 0:
+            print("no qualifications to save")
+        else:
+            save_qualifications_to_db(url, data)
+            save_urls_to_db(url, data)
 
         return data
 
     elif host_name == "careerpointkenya":
         #  use careers point scrapper module
         data = careersPointScrapper.qualifications_scrapping(url)
-        save_to_db(url, data)
+
+        if len(data) == 0:
+            print("no qualifications to save")
+        else:
+            save_qualifications_to_db(url, data)
+            save_urls_to_db(url, data)
 
         return data
 
     elif host_name == "myjobmag":
         # use my job mag scrapper module
         data = jobsMagScrapper.qualifications_scrapping(url)
-        save_to_db(url, data)
+
+        if len(data) == 0:
+            print("no qualifications to save")
+        else:
+            save_qualifications_to_db(url, data)
+            save_urls_to_db(url, data)
+
+        return data
+
+    elif host_name == "jobsinkenya":
+        # use my job mag scrapper module
+        print(f"Scrapping data from {url}")
+        data = jobInKenya.qualifications_scrapping(url)
+
+        if len(data) == 0:
+            print("no qualifications to save")
+        else:
+            save_qualifications_to_db(url, data)
+            save_urls_to_db(url, data)
+
+        return data
+
+    elif host_name == "jobwebkenya":
+        # use my job web kenya scrapper module
+        print(f"Scrapping data from {url}")
+        data = jobWebScrapper.qualifications_scrapping(url)
+
+        if len(data) == 0:
+            print("no qualifications to save")
+        else:
+            save_qualifications_to_db(url, data)
+            save_urls_to_db(url, data)
 
         return data
 
@@ -55,23 +100,46 @@ def scrap_data(url):
         return None
 
 
-def save_to_db(url, data):
+def save_qualifications_to_db(url, data):
+    # save all scrapped qualifications in sqlite db
+
     table = url.split(".")[1]
     db.create_table(table)
     for items in data:
         data = items["Qualification"]
-        db.insert_data(table, data)
-    db.show_data(table)
+        db.insert_qualifications_data(table, data)
 
     return "data inserted"
 
 
-def main():
-    url = get_input()
-    host_name = check_host_name(url)
+def save_urls_to_db(url, data):
 
+    table = url.split(".")[1] + "_url"
+    db.create_table(table)
+    for items in data:
+        data = items["url"]
+        db.insert_url_data(table, data)
+
+    return "data inserted"
+
+
+def main(url):
+
+    host_name = check_host_name(url)
     qualifications = scrap_data(url)
 
 
-if __name__ == "__main__":
-    main()
+url_list = [
+    "https://www.myjobmag.co.ke/",
+    "https://www.brightermonday.co.ke/",
+    "https://www.careerpointkenya.co.ke/",
+    "https://www.jobsinkenya.co.ke/",
+    "https://www.jobwebkenya.com/",
+]
+
+
+schedule.every().day.at("07:30").do(main(url_list[0]))
+schedule.every().day.at("08:00").do(main(url_list[1]))
+schedule.every().day.at("08:30").do(main(url_list[2]))
+schedule.every().day.at("09:00").do(main(url_list[3]))
+schedule.every().day.at("09:30").do(main(url_list[4]))
