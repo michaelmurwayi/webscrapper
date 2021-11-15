0056d9d74f1c9771d423524644759a3b6d30de56
michaelmurwayi <mikemurwayi7@gmail.com>
2021-11-15 08:39:03 +0300

    created load data function to read data from sqlite into a list

--- classifier/load_data.py
+++ classifier/load_data.py
@@ -1,4 +1,7 @@
-from ..common import get_records
+import sys
+
+sys.path.append("..")
+from common import get_records
 
 """
 This Module takes table name
@@ -6,13 +9,37 @@ Reads data from sqlite
 Produces a dataset for all stored data
 """
 
+
 table = "brightermonday"
 
+tables = ["brightermonday", "careerpointkenya", "jobwebkenya"]
 
-def load_data(table):
+
+def load_single_table_data(table):
+    # read data from single table in the database
+
+    data_set = []
     data = get_records(table)
     for index, items in enumerate(data):
-        if len(items) <= 1:
-            data.pop(index)
+        if len(items[0]) > 1:
+            data_set.append(items[0])
 
-    return data
+    return data_set
+
+
+def load_all_table_data(tables):
+    # Takes list of tables and returns data_set of all i
+
+    data_sets = []
+    for table in tables:
+        data = load_single_table_data(table)
+        data_sets.append(data)
+
+    data_set = [ qualification for data in data_sets for qualification in data]
+
+    return data_set
+
+
+# load_single_table_data(table)
+
+load_all_table_data(tables)
